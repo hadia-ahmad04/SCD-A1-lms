2f89e5bb3ac86198f0468d77740942f6f2b198ab
Hadia Ahmad <l216104@lhr.nu.edu.pk>
2023-09-20 00:30:36 +0500

    part c

--- src/main/java/com/mycompany/a1q2b/a1q2b.java
+++ src/main/java/com/mycompany/a1q2b/a1q2b.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.a1q2b;
+
+//package com.mycompany.a1q2b;
 
 import java.io.*;
   
@@ -11,40 +9,61 @@ import java.util.*;
 import java.util.Scanner;
 
  
+abstract class Item {
+    protected int id;
+    protected String title;
 
-class Book {
+    
 
-    private int id;
+    public Item(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
 
-    private String title;
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public abstract String toString();
+}
+
+
+class Book extends Item {
+
+//    private int id;
+//
+//    private String title;
 
     private String author;
 
- 
+    public Book(int id, String title, String a) {
 
-    public Book(int ID, String t, String a) {
-
-        id = ID;
-
-        title = t;
-
+//        id = ID;
+//
+//        title = t;
+        super(id,title);
         author = a;
 
     }
 
-    public int getId() {
-
-        return id;
-
-    }
-
- 
-
-    public String getTitle() {
-
-        return title;
-
-    }
+//    public int getId() {
+//
+//        return id;
+//
+//    }
+//
+// 
+//
+//    public String getTitle() {
+//
+//        return title;
+//
+//    }
 
  
 
@@ -56,11 +75,11 @@ class Book {
 
  
 
-    public void setTitle(String title) {
-
-        this.title = title;
-
-    }
+//    public void setTitle(String title) {
+//
+//        this.title = title;
+//
+//    }
 
  
 
@@ -81,146 +100,233 @@ class Book {
     }
 
 }
+class Magazine extends Item {
+    private List<String> authors;
+    private String publisher;
+    private String publicationDate;
+
+    public Magazine(int id, String title, List<String> authors, String publisher, String publicationDate) {
+        super(id, title);
+        this.authors = authors;
+        this.publisher = publisher;
+        this.publicationDate = publicationDate;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Magazine [ID=" + getId() + ", Title=" + getTitle() + ", Authors=" + authors + ", Publisher=" + publisher + ", Date=" + publicationDate + "]";
+    }
+}
+
+class Newspaper extends Item {
+    private String publisherCompany;
+    private String publicationDate;
+
+    public Newspaper(int id, String title, String publisherCompany, String publicationDate) {
+        super(id, title);
+        this.publisherCompany = publisherCompany;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getPublisherCompany() {
+        return publisherCompany;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Newspaper [ID=" + getId() + ", Title=" + getTitle() + ", Publisher Company=" + publisherCompany + ", Date=" + publicationDate + "]";
+    }
+}
 
  
 
 class Library {
 
-    private List<Book> books;
-
+//    private List<Book> books;
+      private List<Item> items;
  
 
     public Library() {
 
-        this.books = new ArrayList<>();
+        this.items = new ArrayList<>();
 
     }
 
- 
-
-    public void addBook(Book book) {
-
-        books.add(book);
-
+    public void addItem(Item item)
+    {
+        items.add(item);
     }
 
- 
-
-    public void editBook(int id, String title, String author) {
-
-       for (int i = 0; i < books.size(); i++) {
-
-            Book book = books.get(i);
-
-            if (book.getId() == id) {
-
-                book.setTitle(title);
-
-                book.setAuthor(author);
-
-                return;
-
+//    public void addBook(Book book) {
+//
+//        books.add(book);
+//
+//    }
+     public Item getItemById(int id) {
+        for (int i = 0; i < items.size(); i++) {
+            Item item = items.get(i);
+            if (item.getId() == id) {
+                return item;
             }
-
         }
-
-        System.out.println("Book with ID " + id + " not found.");
-
-    }
-
- 
-
-    public void deleteBook(int id) {
-
-        Iterator<Book> iterator = books.iterator();
-
-        while (iterator.hasNext()) {
-
-            Book book = iterator.next();
-
-            if (book.getId() == id) {
-
-                iterator.remove();
-
-                return;
-
-            }
-
-        }
-
-        System.out.println("Book with ID " + id + " not found.");
-
-    }
-
- 
-
-    public Book getBookById(int id) {
-
-        for (int i = 0; i < books.size(); i++) {
-
-            Book book = books.get(i);
-
-            if (book.getId() == id) {
-
-                return book;
-
-            }
-
-        }
-
         return null;
-
+    }
+     
+     public List<Item> getAllItems() {
+        return items;
     }
 
- 
-
-    public List<Book> getAllBooks() {
-
-        return books;
-
-    }
-
- 
-
-    public void loadBooksFromFile(String fileName) {
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-
-                String[] parts = line.split(",");
-
-                if (parts.length >= 3) {
-
-                    int id = Integer.parseInt(parts[0].trim());
-
-                    String title = parts[1].trim();
-
-                    String author = parts[2].trim();
-
- 
-
-                    Book book = new Book(id, title, author);
-
-                    addBook(book);
-
-                }
-
+     
+     public boolean deleteItem(int id) {
+        Iterator<Item> iterator = items.iterator();
+        while (iterator.hasNext()) {
+            Item item = iterator.next();
+            if (item.getId() == id) {
+                iterator.remove();
+                return true;
             }
-
-            System.out.println("Books loaded successfully from " + fileName);
-
-        } catch (IOException e) {
-
-            System.err.println("Error loading books from file: " + e.getMessage());
-
         }
-
+        return false;
     }
-
+     
+     public void displayItem(Item item) {
+        System.out.println(item);
+    }
 }
+
+ 
+
+//    public void editBook(int id, String title, String author) {
+//
+//       for (int i = 0; i < books.size(); i++) {
+//
+//            Book book = books.get(i);
+//
+//            if (book.getId() == id) {
+//
+//                book.setTitle(title);
+//
+//                book.setAuthor(author);
+//
+//                return;
+//
+//            }
+//
+//        }
+//
+//        System.out.println("Book with ID " + id + " not found.");
+//
+//    }
+
+ 
+
+//    public void deleteBook(int id) {
+//
+//        Iterator<Book> iterator = books.iterator();
+//
+//        while (iterator.hasNext()) {
+//
+//            Book book = iterator.next();
+//
+//            if (book.getId() == id) {
+//
+//                iterator.remove();
+//
+//                return;
+//
+//            }
+//
+//        }
+//
+//        System.out.println("Book with ID " + id + " not found.");
+//
+//    }
+
+ 
+
+//    public Book getBookById(int id) {
+//
+//        for (int i = 0; i < books.size(); i++) {
+//
+//            Book book = books.get(i);
+//
+//            if (book.getId() == id) {
+//
+//                return book;
+//
+//            }
+//
+//        }
+//
+//        return null;
+//
+//    }
+//
+// 
+//
+//    public List<Book> getAllBooks() {
+//
+//        return books;
+//
+//    }
+
+ 
+
+//    public void loadBooksFromFile(String fileName) {
+//
+//        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+//
+//            String line;
+//
+//            while ((line = reader.readLine()) != null) {
+//
+//                String[] parts = line.split(",");
+//
+//                if (parts.length >= 3) {
+//
+//                    int id = Integer.parseInt(parts[0].trim());
+//
+//                    String title = parts[1].trim();
+//
+//                    String author = parts[2].trim();
+//
+// 
+//
+//                    Book book = new Book(id, title, author);
+//
+//                    addBook(book);
+//
+//                }
+//
+//            }
+//
+//            System.out.println("Books loaded successfully from " + fileName);
+//
+//        } catch (IOException e) {
+//
+//            System.err.println("Error loading books from file: " + e.getMessage());
+//
+//        }
+//
+//    }
+//
+//}
 
  
 
@@ -229,7 +335,7 @@ public class a1q2b {
     public static void main(String[] args) {
 
         Library library = new Library();
-        library.loadBooksFromFile("data.txt");
+//        library.loadBooksFromFile("data.txt");
 
         Scanner scanner = new Scanner(System.in);
 
@@ -283,11 +389,11 @@ public class a1q2b {
 
  
 
-                    Book newBook = new Book(id, title, author);
+                    Item newItem = new Book(id, title, author);
 
-                    library.addBook(newBook);
+                    library.addItem(newItem);
 
-                    System.out.println("Book added successfully.");
+                    System.out.println("Item added successfully.");
 
                     break;
 
@@ -299,9 +405,9 @@ public class a1q2b {
 
                     scanner.nextLine();
 
-                    Book bookToEdit = library.getBookById(editId);
+                    Item editItem = library.getItemById(editId);
 
-                    if (bookToEdit != null) {
+                    if (editItem != null) {
 
                         System.out.print("Enter new Book Title: ");
 
@@ -311,7 +417,7 @@ public class a1q2b {
 
                         String newAuthor = scanner.nextLine();
 
-                        library.editBook(editId, newTitle, newAuthor);
+//                        library.editBook(editId, newTitle, newAuthor);
 
                         System.out.println("Book edited successfully.");
 
@@ -331,11 +437,13 @@ public class a1q2b {
 
                     scanner.nextLine();
 
-                    Book bookToDelete = library.getBookById(deleteId);
+//                    Book bookToDelete = library.deleteItem(deleteId);
+                     boolean deleteItem=library.deleteItem(deleteId);
 
-                    if (bookToDelete != null) {
+                    if (deleteItem) {
 
-                        library.deleteBook(deleteId);
+//                        library.deleteBook(deleteId);
+                         library.deleteItem(deleteId);
 
                         System.out.println("Book deleted successfully.");
 
@@ -349,9 +457,10 @@ public class a1q2b {
 
                 case 4:
                     System.out.println("Book Details:");
-                    for (Book bookToView : library.getAllBooks()) 
+//                    for (Book bookToView : library.getAllBooks()) 
+                    for (Item viewItem : library.getAllItems()) 
                     {
-                        System.out.println(bookToView);  
+                        System.out.println(viewItem);  
                     } 
 
                     break;
@@ -366,13 +475,14 @@ public class a1q2b {
 
  
 
-                    Book bookToView = library.getBookById(viewId);
+//                    Book bookToView = library.getBookById(viewId);
+                    Item IdView = library.getItemById(viewId);
 
-                    if (bookToView != null) {
+                    if (IdView != null) {
 
                         System.out.println("Book Details:");
 
-                        System.out.println(bookToView);
+                        System.out.println(IdView);
 
                     } else {
 
@@ -388,7 +498,8 @@ public class a1q2b {
 
                     String fileName = scanner.nextLine();
 
-                    library.loadBooksFromFile(fileName);
+//                    library.loadBooksFromFile(fileName);
+                    
 
                     break;
 
